13876c414dc6161633d3fc1c255f7f444f928a2e
kzrv <91475730+kzrv@users.noreply.github.com>
2022-06-27 21:44:34 +0200

    Fixed some mistakes

--- src/main/java/cz/kzrv/library/util/Validation.java
+++ src/main/java/cz/kzrv/library/util/Validation.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+
 @Component
 public class Validation implements Validator {
     private final PersonDao personDao;
